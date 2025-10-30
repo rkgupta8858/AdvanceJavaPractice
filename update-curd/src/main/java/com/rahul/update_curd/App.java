c43de1045c5588fc15f1cf7580870d59bfb67a46
rkgupta8858 <rahulgupta8858@gmail.com>
2025-10-30 10:55:18 +0530

    first commit

---
+++ update-curd/src/main/java/com/rahul/update_curd/App.java
@@ -0,0 +1,22 @@
+package com.rahul.update_curd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App 
+{
+    public static void main( String[] args ) throws ClassNotFoundException, SQLException
+    {
+    		String url = "jdbc:mysql://localhost:3306/emp";
+    		String userName = "root";
+    		String userpass = "root";
+       Class.forName("com.mysql.cj.jdbc.Driver");
+       Connection connection = DriverManager.getConnection(url, userName, userpass);
+       Statement statement = connection.createStatement();
+       int count = statement.executeUpdate("update employeedetails set name='Ravi Gupta' where id=105");
+       System.out.println(count +" Record Update");
+       
+    }
+}
